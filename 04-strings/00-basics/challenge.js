15eff4612063eaa650891e30bfec01039f31ee1a
heather <hlahaie@gmail.com>
2014-11-24 20:28:29 -0700

    assign 04-strings ex0

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,19 +1,27 @@
 module.exports.formLetter = function(firstName, senderName, message) {
-
+  var result = "Hello " + firstName + ",\n\n" + message + "\n\nSincerely,\n" + senderName;
+  return (result);
 };
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
-
+  var result = bigString.slice(startA, endA) + bigString.slice(startB, endB);
+  return (result);
 };
 
 module.exports.findFirstMatch = function(text, searchString) {
-
+  var result = text.indexOf(searchString);
+  return(result);
 };
 
 module.exports.findLastMatch = function(text, searchString) {
-
+  var result = text.lastIndexOf(searchString);
+  return(result);
 };
 
 module.exports.substringBetweenMatches = function(text, searchString) {
-
+  var firstSearchString = text.indexOf(searchString);
+  var lastSearchString = text.lastIndexOf(searchString);
+  var beginingWordString = searchString.length;
+  var result = text.slice(firstSearchString + beginingWordString, lastSearchString);
+  return(result);
 };
