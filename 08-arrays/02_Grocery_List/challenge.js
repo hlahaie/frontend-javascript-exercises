bba02da99ef31ff4780beb9a489f2b569cc14866
heather <hlahaie@gmail.com>
2014-12-03 16:34:25 -0700

    assign 08-array ex2

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,18 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function(newItem, list) {
+  if (list.indexOf(newItem) >= 0) {
+    var groceryList = list;
+    return groceryList;
+  }
+  else {
+    var groceryList = list;
+    groceryList.push(newItem);
+    return groceryList;
+  }
+}
 
-module.exports.reverseSortedList = undefined;
+module.exports.reverseSortedList = function(list) {
+  var newList = list;
+  newList.sort();
+  newList.reverse();
+  return newList;
+}
