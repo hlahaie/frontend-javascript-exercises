47ca0b7f2c800d77989a2d92ab3ef03fc9cdf706
heather <hlahaie@gmail.com>
2014-12-02 21:20:59 -0700

    assign 05-booleans ex0

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,24 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+  var compareStrings = stringOne == stringTwo;
+  return compareStrings;
 };
 
 module.exports.notEqual = function(one, two) {
-
+  var compareValues = one !== two;
+  return compareValues;
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+  var compareMiddle = lower < middle && middle < upper;
+  return compareMiddle;
 };
 
 module.exports.outsideRanges = function(number) {
-
+  var compareOutside = (10 > number || number > 20) && (42 >= number || number > 75) && (1 >= number || number >= 6);
+  return compareOutside;
 };
 
 module.exports.nameAndPrice = function(name, price) {
-
+  var compareNamdAndPrice = (name == "NYTimes" || name == "LATimes") && (price >= 1);
+  return compareNamdAndPrice;
 };
