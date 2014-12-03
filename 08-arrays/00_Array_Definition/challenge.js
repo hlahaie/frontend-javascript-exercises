515eaa55cf9b2a00c9f1b5a7590d756cf971567f
heather <hlahaie@gmail.com>
2014-12-02 21:24:59 -0700

    assign 08-arrays ex0

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,9 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function (indexOne, indexTwo, indexThree, indexFour) {
+  var resultArray = [indexOne, indexTwo, indexThree, indexFour];
+  return resultArray;
+}
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function (arrayOne) {
+  var newArray = [arrayOne[0], arrayOne[arrayOne.length-1]];
+  return newArray;
+}
