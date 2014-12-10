09cda2286df7b751dc8d955e28a97046de5b9900
heather <hlahaie@gmail.com>
2014-12-09 21:18:11 -0700

    assign 09-objects ex0

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,38 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+  var courseInfo = {
+      title: courseTitle,
+      duration: courseDuration,
+      students: courseStudents
+};
+return courseInfo;
+}
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+  var addToObject = object;
+  if (!addToObject.hasOwnProperty(newProp)) { 
+    addToObject[newProp] = newValue;
+  }
+  // if (addToObject[newProp] === undefined) {
+  //   addToObject[newProp] = newValue;
+  // }
+  return addToObject;
+}
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter) {
+  var combineLetter = "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
+  return combineLetter;
+}
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+  //Construct a hash
+
+  var productHash = {
+    'macbook air': 999,
+    'macbook pro': 1299,
+    'mac pro': 2499,
+    'apple sticker': 1
+  };
+
+  return productHash[item.toLowerCase()] <= money;
+
+}
