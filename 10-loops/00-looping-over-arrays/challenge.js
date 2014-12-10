2348793fc8f9749c7a3f469dad1a0e0ed6dd1c09
heather <hlahaie@gmail.com>
2014-12-09 21:19:33 -0700

    assign 10-loops ex0

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,23 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(number) {
+  var addNumbers = 0;
+  for (var counter = 0; counter < number.length; counter++) {
+    addNumbers = addNumbers + number[counter];
+  }
+  return addNumbers;
+}
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function(inputString) {
+ var changeString = inputString.split(",");
+ for (var counter = 0; counter < changeString.length; counter++) {
+    changeString[counter] = changeString[counter].toLowerCase();
+ }
+ return changeString;
+}
 
-module.exports.addIndex = undefined;
+module.exports.addIndex = function(initialData) {
+  var addToString = [];
+  for (var counter = 0; counter < initialData.length; counter++) {
+    addToString[counter] = counter + " is " + initialData[counter].toString();
+  }
+  return addToString;
+}
