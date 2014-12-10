ec1ce748ea16669567a8a555f9e66890ecaf54dd
heather <hlahaie@gmail.com>
2014-12-09 21:25:23 -0700

    assign 10-loops ex2

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,23 @@
-module.exports.copy = undefined;
+module.exports.copy = function(original) {
+  var copyValue = {};
+  for (var key in original) {
+    copyValue[key] = original[key];
+  }
+  return copyValue;
+}
 
-module.exports.extend = undefined;
+module.exports.extend = function(dest, src) {
+  for (var key in src) {
+      dest[key] = src[key];
+      }
+  return dest;
+}
 
-module.exports.hasElems = undefined;
+module.exports.hasElems = function(hash, arrayToCompare) {
+  for (counter = 0; counter < arrayToCompare.length; counter++) {
+    if (!hash.hasOwnProperty(arrayToCompare[counter])) {
+      return false;
+    }
+  }
+  return true;
+}
