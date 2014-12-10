6277b5a8ae838dfe15b07c6d0c4b5b6098b6478e
heather <hlahaie@gmail.com>
2014-12-09 21:22:39 -0700

    assign 10-loops ex1

--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -1,5 +1,25 @@
-module.exports.getKeys = undefined;
+module.exports.getKeys = function(keys) {
+  var keyProperty = [];
+  for (var key in keys) {
+    keyProperty.push(key);
+  }
 
-module.exports.getValues = undefined;
+  // var keyProperty = Object.keys(keys);
+  return keyProperty;
+}
 
-module.exports.objectToArray = undefined;
+module.exports.getValues = function(objectValue) {
+  var valueProperty = [];
+  for (var name in objectValue) {
+    valueProperty.push(objectValue[name]);
+  }
+  return valueProperty;
+}
+
+module.exports.objectToArray = function(arrayValue) {
+  var keyIsValue = [];
+  for (var keys in arrayValue) {
+    keyIsValue.push(keys + " is " + arrayValue[keys]);
+  }
+  return keyIsValue;
+}
